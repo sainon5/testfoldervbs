e3c5de82c4872aabf67d1aab9ef61f3216e5b479
sainon5 <64122028+sainon5@users.noreply.github.com>
2020-04-23 15:43:30 +1000

    feat(utils): add log and add functions

--- app/utils.ts
+++ app/utils.ts
@@ -5,8 +5,15 @@ export const capitalise = (word: string) => {
 export const createLine = (entity: string, describingWord: string) => {
     return `Never ${capitalise(describingWord)} a ${entity}`;
 };
-
+ export const log = (message: string) => {
+     console.info(`test ${message}`)
+ }
+export const add = (firstValue: number, secondValue?: number) =>{
+    const steve = firstValue + (secondValue ?? 20);
+    return steve
+}
 export default {
    createLine,
-   capitalise
+   capitalise,
+   log
 };
